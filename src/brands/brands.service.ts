c6c4e69790d12bacb2f80d72b21d7d722cb064ff
sh4rkd <20006132@es.uveg.edu.mx>
2023-07-08 08:15:16 -0700

    Seed de cars y brands

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -6,11 +6,12 @@ import { v4 as uuid} from 'uuid';
 
 @Injectable()
 export class BrandsService {
-  private brands: Brand[] = [{
-      id: uuid(),
-      name: "Toyota",
-      createdAt: new Date().getTime()
-    }
+  private brands: Brand[] = [
+    // {
+    //   id: uuid(),
+    //   name: "Toyota",
+    //   createdAt: new Date().getTime()
+    // }
   ];
 
   create(createBrandDto: CreateBrandDto) {
@@ -49,4 +50,10 @@ export class BrandsService {
   remove(id: string) {
     this.brands =  this.brands.filter(brand => brand.id !== id);
   }
+
+  fillBrandsWithSeedData(brands :Brand[]){
+      this.brands = brands;
+  }
 }
+
+
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -6,21 +6,11 @@ import { CreateCarDto, UpdateCarDto } from './dto';
 @Injectable()
 export class CarsService {    
     private cars: Car[] = [
-        {
-            id: uuid(),
-            brand: "Toyota",
-            model: "Corolla"
-        },
-        {
-            id: uuid(),
-            brand: "Honda",
-            model: "Civic"
-        },
-        {
-            id: uuid(),
-            brand: "Jeep",
-            model: "Cherokee"
-        }
+        // {
+        //     id: uuid(),
+        //     brand: "Toyota",
+        //     model: "Corolla"
+        // },
     ]
 
     findAll(){
@@ -62,5 +52,9 @@ export class CarsService {
     delete(id: string){
         this.findOneById(id)
         this.cars = this.cars.filter(car => car.id !== id)
+    }
+
+    fillCarsWithSeedData(cars :Car[]){
+        this.cars = cars
     }
 }
